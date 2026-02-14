c43d5ed1a1f0b0f4629bc2ae0cf293c2c56139e5
AtifRehman47 <atifrehman401@gmail.com>
2026-02-13 23:26:30 -0800

    function with obj

---
+++ app.js
@@ -0,0 +1,32 @@
+// object using new keyword 
+
+// function Person(name,from,age){
+//         this.name = name
+//         this.from = from
+//         this.age = age
+// }
+
+// let show = new Person('Atif','karachi',20)
+// console.log(show.name,show.from,show.age);
+
+
+// function std1 (name,age,qualification){
+//     this.name = name
+//     this.age = age
+//     this.qualification = qualification
+// }
+
+// let show = new std1('Atif',20,'BBIT');
+// console.log(show.name,show.age,show.qualification);
+
+
+// function stdOneData (name,marks){
+//         this.name = name
+//         this.marks = marks
+// }
+
+// let show = new stdOneData('AtifRehman',90)
+// let show2 = new stdOneData('Asim',80)
+// console.log(show.name,show.marks);
+// console.log(show2.name,show2.marks);
+
